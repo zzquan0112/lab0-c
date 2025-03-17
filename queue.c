444d0832e4cb323e45bd8bab7b82a6dd4c537cbc
zzq <zzquan0112@gmail.com>
2025-03-18 06:03:30 +0800

    Add size function
    Implement q_size function to return the current number of elements in
    the queue.
    
    Change-Id: I3f25818263ad1011aeda5b8a7e597abf095fe178

--- queue.c
+++ queue.c
@@ -83,7 +83,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *list;
+
+    list_for_each(list, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
